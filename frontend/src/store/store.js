0c2dc100e33cfd610d0f4e394ef2ac6cbec71bd0
ofek ashkenazi <ofeka18@gmail.com>
2023-01-11 23:42:07 +0200

    fixing problems for deploay redux devtools and new build

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,7 @@
 import { combineReducers, legacy_createStore as createStore } from 'redux'
 import { toyReducer } from './toy.reducer'
 
-const middleware = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+const middleware = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose
 
 const rootReducer = combineReducers({
     toyModule: toyReducer,
